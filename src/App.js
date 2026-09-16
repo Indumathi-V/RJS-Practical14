eeb209df45143726a8a5cb9b0ea8b16c0b0de0e5
Indumathi-V <54302074+Indumathi-V@users.noreply.github.com>
2026-09-16 11:21:05 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+
+    // TODO 1:
+    // Create a state variable called message
+    // Initial value should be an empty string.
+
+
+    // TODO 2:
+    // Use useEffect() to display the message
+    // "Welcome! The page has loaded successfully."
+    // when the component loads.
+
+
+    return (
+        <div>
+            <h1>React useEffect Practical</h1>
+
+            <p id="message">
+                {/* TODO 3:
+                    Display the message here
+                */}
+            </p>
+        </div>
+    );
+}
+
+export default App;
